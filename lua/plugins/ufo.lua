d98ecebe39a4de34561369c80abd4470347a4dc4
Tristan <zhenquan.me@gmail.com>
2025-11-20 19:22:21 +0800

    feat(plugins/ufo): remove keymaps zR/zM/zr/zm (Neovim supports them natively)

--- lua/plugins/ufo.lua
+++ lua/plugins/ufo.lua
@@ -17,10 +17,6 @@ return {
 
     -- Lazy keymaps that call ufo APIs
     keys = {
-      { "zR", function() require("ufo").openAllFolds() end, desc = "UFO: open all folds" },
-      { "zM", function() require("ufo").closeAllFolds() end, desc = "UFO: close all folds" },
-      { "zr", function() require("ufo").openFoldsExceptKinds() end, desc = "UFO: open folds except kinds" },
-      { "zm", function() require("ufo").closeFoldsWith() end, desc = "UFO: close folds with level" },
       {
         "K",
         function()
