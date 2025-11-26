769f641cb830c90d3d0651ebf0f11d2503244ed6
Tristan <zhenquan.me@gmail.com>
2025-11-26 20:10:40 +0800

    feat(plugins/lazydev): initial plugin setup

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua",  -- only load when editing Lua files
+    opts = {
+      library = {
+        -- optional extra libraries; can be empty at first
+      },
+    },
+  },
+}
+
