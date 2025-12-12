04c249640bc765e6ad001fe66243de9699f3b5e5
Tristan <zhenquan.me@gmail.com>
2025-12-12 19:50:16 +0800

    feat(plugins/multicursor): update the keybindings, using "m" as a multi prefix.

--- lua/plugins/multicursor.lua
+++ lua/plugins/multicursor.lua
@@ -15,10 +15,10 @@ return {
       set({ "n", "x" }, "<leader><down>", function() mc.lineSkipCursor(1) end)
 
       -- Add or skip adding a new cursor by matching word/selection
-      set({ "n", "x" }, "<leader>n", function() mc.matchAddCursor(1) end)
-      set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
-      set({ "n", "x" }, "<leader>N", function() mc.matchAddCursor(-1) end)
-      set({ "n", "x" }, "<leader>S", function() mc.matchSkipCursor(-1) end)
+      set({ "n", "x" }, "<leader>mn", function() mc.matchAddCursor(1) end)
+      set({ "n", "x" }, "<leader>ms", function() mc.matchSkipCursor(1) end)
+      set({ "n", "x" }, "<leader>mN", function() mc.matchAddCursor(-1) end)
+      set({ "n", "x" }, "<leader>mS", function() mc.matchSkipCursor(-1) end)
 
       -- Add and remove cursors with control + left click.
       set("n", "<c-leftmouse>", mc.handleMouse)
