bffe583ab2bda8259a2736433d38333f481b059e
perastra.cc <zhenquan.me@gmail.com>
2026-02-18 19:47:01 +0800

    chore(plugins/treesitter): format config

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,19 +2,34 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function () 
+    config = function()
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-          ensure_installed = { 
-          "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html",
-          "css", "latex", "norg", "scss", "svelte", "tsx", "typst", "vue",
-          "regex"
-         },
-          sync_install = false,
-          highlight = { enable = true },
-          indent = { enable = true },  
-        })
-    end
-  }
- }
+        ensure_installed = {
+          "c",
+          "lua",
+          "vim",
+          "vimdoc",
+          "query",
+          "elixir",
+          "heex",
+          "javascript",
+          "html",
+          "css",
+          "latex",
+          "norg",
+          "scss",
+          "svelte",
+          "tsx",
+          "typst",
+          "vue",
+          "regex",
+        },
+        sync_install = false,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+}
