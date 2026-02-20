29f07f14e1955f55cc7f5ea50fe20129cf847149
perastra.cc <zhenquan.me@gmail.com>
2026-02-20 19:03:19 +0800

    feat(plugins/mini-surround): Remap keybindings to follow the LazyVim-style mappings

--- lua/plugins/mini-surround.lua
+++ lua/plugins/mini-surround.lua
@@ -1,7 +1,24 @@
 return {
+
   {
     "echasnovski/mini.surround",
-    version = "*",
-    opts = {},
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy", -- Loads the plugin after startup so mappings like 'ys' are available
+
+    opts = {
+      mappings = {
+        add = "gsa", -- Add surrounding in Normal and Visual mode
+        delete = "gsd", -- Delete surrounding
+        find = "gsf", -- Find right surrounding
+        find_left = "gsF", -- Find left surrounding
+        highlight = "gsh", -- Highlight surrounding
+        replace = "gsr", -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+
+        suffix_last = "l", -- Suffix to search with "prev" method
+        suffix_next = "n", -- Suffix to search with "next" method
+      },
+      search_method = "cover_or_next",
+    },
   },
 }
