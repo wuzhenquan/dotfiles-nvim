9b2d0168b2abb5e00d516d16892bb6897b471382
Tristan <zhenquan.me@gmail.com>
2026-05-13 20:59:42 +0800

    feat(plugins/snacks): trim dashboard items

--- lua/plugins/snacks/dashboard.lua
+++ lua/plugins/snacks/dashboard.lua
@@ -20,9 +20,6 @@ return {
           -- When using a function, the `items` argument are the default keymaps.
           ---@type snacks.dashboard.Item[]
           keys = {
-            { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
-            { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
-            { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
             { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
             {
               icon = " ",
@@ -32,7 +29,6 @@ return {
             },
             { icon = " ", key = "s", desc = "Restore Session", section = "session" },
             { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
-            { icon = " ", key = "q", desc = "Quit", action = ":qa" },
           },
           -- Used by the `header` section
           header = [[
